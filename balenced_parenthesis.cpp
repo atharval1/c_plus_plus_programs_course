be11e65cc812f19827ea7668cfa9475b5f101ec9
atharva labhasetwar <atharvalabhasetwar1234@gmail.com>
2021-07-11 00:03:59 +0530

    balenced parenthesis

---
+++ balenced_parenthesis.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+bool isValid(string s)
+{
+    int n = s.size();
+
+    stack<char> st;
+    bool ans = true;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '{' or s[i] == '(' or s[i] == '[')
+        {
+            st.push(s[i]);
+        }
+
+        else if (s[i] == ')')
+        {
+            if (!st.empty() and st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+
+        else if (s[i] == ']')
+        {
+            if (!st.empty() and st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+
+        else if (s[i] == '}')
+        {
+            if (!st.empty() and st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+    }
+    if (!st.empty())
+    {
+        return false;
+    }
+    else
+    {
+        return ans;
+    }
+}
+int main()
+{
+    string s = "{()[]}";
+
+    if (isValid(s))
+    {
+        cout << "valid string " << endl;
+    }
+    else
+    {
+        cout << "invalid String " << endl;
+    }
+    return 0;
+}
